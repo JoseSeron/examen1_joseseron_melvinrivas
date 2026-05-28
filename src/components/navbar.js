60a7ca3a9d741e757edc9fd96776afc331245f2f
JoseSeron <joserobertoseron@gmail.com>
2026-05-28 15:22:14 -0600

    component navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <button className="btn">NETFLIX</button>
+            <button className="btn">Home</button>
+            <button className="btn">Shows</button>
+            <button className="btn">Movies</button>
+            <button className="btn">Games</button>
+            <button className="btn">News&Popular</button>
+            <button className="btn">My List</button>
+            <button className="btn">Browse by Languages</button>
+        </nav>
+    )
+}
+export default Navbar
