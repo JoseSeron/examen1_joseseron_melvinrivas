a6207df4def65d1860b01f0260c95d1999f579c9
JoseSeron <joserobertoseron@gmail.com>
2026-05-28 17:39:15 -0600

    navbar a medio palo, falta  meterle sazon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/navbar';
 
@@ -6,10 +6,9 @@ import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar /> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
+       
       </header>
     </div>
   );
