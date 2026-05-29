d6fe8768d527bb77e88c9c082082dd3f97b87527
JoseSeron <joserobertoseron@gmail.com>
2026-05-28 21:47:58 -0600

    destacado.css destacado.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import demon_slayer from './demon_slayer.gif';
+
 import './App.css';
 import Navbar from './components/navbar';
-
+import Destacado from './components/destacado';
 
 function App() {
   return (
     <div className="App" >
+
       <Navbar />
-      <header className="App-header">
-        <img src={demon_slayer} className="App-demon_slayer" alt="demon_slayer" />
+      <Destacado />
       
-        <h1>Demon Slayer</h1>
-
-        <p>
-          Tanjiro Kamado se une al Cuerpo de Exterminio de Demonios
-          para salvar a su hermana y vengar a su familia.
-        </p>
-
-        <div className="banner-buttons">
-          <button className="play-btn">▶ Reproducir</button>
-          <button className="info-btn">ℹ Más información</button>
-          
-        </div>
-        </header>
       </div>
   );
 }
---
+++ src/components/destacado.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './destacado.css'
+import demon_slayer from '../demon_slayer.gif';
+
+const Destacado = () => {
+  return (
+    <div className="destacado-container">
+        <img src={demon_slayer} className="App-demon_slayer" alt="demon_slayer" />
+      
+        <h1>Demon Slayer</h1>
+
+        <p>
+          Tanjiro Kamado se une al Cuerpo de Exterminio de Demonios
+          para salvar a su hermana y vengar a su familia.
+        </p>
+
+        <div className="banner-buttons">
+          <button className="play-btn">▶ Reproducir</button>
+          <button className="info-btn">ℹ Más información</button>
+          
+        </div>
+        </div>
+  )
+}
+
+export default Destacado
