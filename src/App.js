218a9ddd8bc6d1b9ec1436777a0a01fe2ba823bb
Melnite22 <l33061415@gmail.com>
2026-05-28 21:10:38 -0600

    portado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import logo from './logo.svg';
+import demon_slayer from './demon_slayer.gif';
 import './App.css';
 import Navbar from './components/navbar';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={demon_slayer} className="App-demon_slayer" alt="demon_slayer" />
+      
+        <h1>Demon Slayer</h1>
 
-      </header>
-    </div>
+        <p>
+          Tanjiro Kamado se une al Cuerpo de Exterminio de Demonios
+          para salvar a su hermana y vengar a su familia.
+        </p>
+
+        <div className="banner-buttons">
+          <button className="play-btn">▶ Reproducir</button>
+          <button className="info-btn">ℹ Más información</button>
+          
+        </div>
+        </header>
+      </div>
   );
 }
 
